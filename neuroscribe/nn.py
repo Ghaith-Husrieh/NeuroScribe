551c32342731caf2703ec630da6bbe997ed2d3a3
Ghaith Husrieh <Ghaith.Husrieh.01@gmail.com>
2024-04-04 01:38:48 +0300

    Add parameter access, device switching, and mode toggle methods to Module class

--- neuroscribe/nn.py
+++ neuroscribe/nn.py
@@ -3,15 +3,51 @@ from neuroscribe.tensor import Tensor
 
 class Module:
     def __init__(self):
-        self.training = True  # Note: used when implementing batchnorm and dropout layers
+        self._training = False
+        self._device = 'cpu'
+        self._parameters = {}
+        self._submodules = {}
 
     def __call__(self, *input):
         return self.forward(*input)
 
-    def parameters(self):
-        return []
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+        if isinstance(value, Module):
+            self._submodules[name] = self.__dict__[name]
+        elif isinstance(value, Tensor):
+            self._parameters[name] = self.__dict__[name]
 
     def forward(self, *input): raise NotImplementedError
+
+    def parameters(self):
+        for _, param in self._parameters.items():
+            yield param
+        for _, module in self._submodules.items():
+            yield from module.parameters()
+
+    def to(self, device):
+        self._device = device
+        for _, module in self._submodules.items():
+            module.to(device)
+        for name, param in self._parameters.items():
+            param = param.to(device)
+            self._parameters[name] = param
+            self.__dict__[name] = param
+        return self
+
+    def _mode(self, training):
+        self._training = training
+        for _, module in self._submodules.items():
+            module._mode(training)
+        for _, param in self._parameters.items():
+            param.requires_grad = training
+
+    def train(self):
+        self._mode(training=True)
+
+    def eval(self):
+        self._mode(training=False)
 
 
 class Linear(Module):
@@ -22,10 +58,7 @@ class Linear(Module):
         self.weight = Tensor.randn(out_features, in_features)
         self.bias = Tensor.randn(out_features)
 
-    def parameters(self):
-        return [self.weight] + [self.bias]
-
     def forward(self, x):
         if not isinstance(x, Tensor):
-            x = Tensor(x)
+            x = Tensor.create(x, requires_grad=self._training, device=self._device)
         return x.matmul(self.weight.transpose()) + self.bias
