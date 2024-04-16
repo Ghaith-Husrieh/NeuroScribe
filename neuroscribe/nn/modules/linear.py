9ade2d25a77a0da72de383213459e7302b09c39d
Ghaith Husrieh <Ghaith.Husrieh.01@gmail.com>
2024-04-16 21:02:16 +0300

    Add the ability to disable bias addition in Linear layer

--- neuroscribe/nn/modules/linear.py
+++ neuroscribe/nn/modules/linear.py
@@ -4,14 +4,17 @@ from .module import Module
 
 
 class Linear(Module):
-    def __init__(self, in_features, out_features):
+    def __init__(self, in_features, out_features, bias=True):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.weight = Tensor.randn(out_features, in_features)
-        self.bias = Tensor.randn(out_features)
+        self.bias = Tensor.randn(out_features) if bias else None
 
     def forward(self, x):
         if not isinstance(x, Tensor):
             x = Tensor.create(x, requires_grad=self._training, device=self._device)
-        return x @ self.weight.T + self.bias
+
+        if self.bias is not None:
+            return x @ self.weight.T + self.bias
+        return x @ self.weight.T
