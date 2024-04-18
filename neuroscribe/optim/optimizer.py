edc81f7d8780a25158c7bef6d067cfafe1b99f67
Ghaith Husrieh <Ghaith.Husrieh.01@gmail.com>
2024-04-18 20:45:09 +0300

    Enhance parameter handling in Optimizer class to support single Tensor or iterable of Tensors with validation

--- neuroscribe/optim/optimizer.py
+++ neuroscribe/optim/optimizer.py
@@ -1,6 +1,13 @@
+from neuroscribe.tensor import Tensor
+
+
 class Optimizer:
     def __init__(self, params):
-        self.params = params
+        self.params = [params] if isinstance(params, Tensor) else list(params)
+        if not all(isinstance(param, Tensor) for param in self.params):
+            raise TypeError("The params argument must be a Tensor or an iterable of Tensors")
+        if len(self.params) == 0:
+            raise ValueError("Optimizer received an empty parameter list")
 
     def zero_grad(self):
         for param in self.params:
