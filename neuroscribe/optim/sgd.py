12b88d18a5a463b6a151a6e287555546d17f014a
Ghaith Husrieh <Ghaith.Husrieh.01@gmail.com>
2024-05-04 00:07:44 +0300

    Add momentum support to SGD optimizer

--- neuroscribe/optim/sgd.py
+++ neuroscribe/optim/sgd.py
@@ -1,12 +1,24 @@
+from neuroscribe.tensor import Tensor
+
 from .optimizer import Optimizer
 
 
-# TODO: Add support for momentum
 class SGD(Optimizer):
-    def __init__(self, params, lr=0.001):
+    def __init__(self, params, lr=0.001, momentum=0):
         super().__init__(params)
+        if lr < 0:
+            raise ValueError(f"Invalid learning rate: {lr}")
         self.lr = lr
+        if momentum < 0:
+            raise ValueError(f"Invalid momentum value: {momentum}")
+        self.momentum = momentum
+        self.velocities = [Tensor.zeros_like(param) for param in self.params] if self.momentum != 0 else None
 
     def step(self):
-        for param in self.params:
-            param.data = param.data - self.lr * param.grad.data
+        if self.momentum == 0:
+            for param in self.params:
+                param.data = param.data - self.lr * param.grad.data
+        else:
+            for i, param in enumerate(self.params):
+                self.velocities[i].data = self.momentum * self.velocities[i].data - self.lr * param.grad.data
+                param.data = param.data + self.velocities[i].data
