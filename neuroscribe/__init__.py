c3393226333464d27540640bb30822258b63450f
Ghaith Husrieh <Ghaith.Husrieh.01@gmail.com>
2024-05-03 18:55:42 +0300

    Move functional operations to separate module and organize utils directory

--- neuroscribe/__init__.py
+++ neuroscribe/__init__.py
@@ -1,26 +1,7 @@
 from neuroscribe.nn.modules.module import load, save
 from neuroscribe.tensor import Tensor
+from neuroscribe.utils._utils._functional_api import *
+from neuroscribe.version import __git_revision__, __version__
 
-from .version import __git_revision__, __version__
-
-__all__ = ['tensor', 'zeros', 'ones', 'randn', 'empty', 'save', 'load']
-
-
-def tensor(data, dtype='float32', requires_grad=False, device='cpu'):
-    return Tensor.create(data, dtype=dtype, requires_grad=requires_grad, device=device)
-
-
-def zeros(shape, dtype='float32', requires_grad=False, device='cpu'):
-    return Tensor.zeros(shape, dtype=dtype, requires_grad=requires_grad, device=device)
-
-
-def ones(shape, dtype='float32', requires_grad=False, device='cpu'):
-    return Tensor.ones(shape, dtype=dtype, requires_grad=requires_grad, device=device)
-
-
-def randn(*shape, dtype='float32', requires_grad=False, device='cpu'):
-    return Tensor.randn(*shape, dtype=dtype, requires_grad=requires_grad, device=device)
-
-
-def empty(*shape, dtype='float32', requires_grad=False, device='cpu'):
-    return Tensor.empty(*shape, dtype=dtype, requires_grad=requires_grad, device=device)
+__all__ = ['Tensor', 'tensor', 'zeros', 'ones', 'randn', 'empty', 'save',
+           'load', 'add', 'sub', 'mul', 'matmul', 'relu', 'mean', 'square']
---
+++ neuroscribe/utils/_utils/_functional_api.py
@@ -0,0 +1,21 @@
+from neuroscribe.tensor import Tensor
+
+__all__ = ['tensor', 'zeros', 'ones', 'randn', 'empty',
+           'add', 'sub', 'mul', 'matmul', 'relu', 'mean', 'square']
+
+# Tensor Static Methods
+tensor = Tensor.create
+zeros = Tensor.zeros
+ones = Tensor.ones
+randn = Tensor.randn
+empty = Tensor.empty
+
+
+# Tensor Instance Methods
+def relu(input): return input.relu()
+def mean(input): return input.mean()
+def square(input): return input.square()
+def add(input, other): return input.add(other)
+def sub(input, other): return input.sub(other)
+def mul(input, other): return input.mul(other)
+def matmul(input, other): return input.matmul(other)
